a1041351c99ad06e0c47074fcac9e5d01635f1f7
Jason Breck <jbreck@cs.wisc.edu>
2019-08-12 15:44:27 -0500

    This compiles

---
+++ cfind/cfind.ml
@@ -0,0 +1,36 @@
+(*open Core
+open CfgIr
+open Apak*)
+open Srk
+(*open Safety*)
+
+(* Frontends *)
+(*open TranslateCil
+open TranslateCbp
+open Conversion*)
+
+(* Analyses *)
+(*open Cra
+open Proofspace
+open Dependence
+open ConcDep
+open Chora*)
+
+let usage_msg = "Cfind program analyzer\nUsage: cfind [OPTIONS] file.smt2"
+
+let anon_fun s = ignore (CmdLine.parse s)
+
+let () =
+  (*Sys.set_signal Sys.sigtstp (Sys.Signal_handle (fun _ -> Log.print_stats ()));*)
+  let spec_list = CmdLine.spec_list () in
+  Arg.parse (Arg.align spec_list) anon_fun usage_msg
+  (*;
+  match !CfgIr.gfile with
+  | None -> begin
+      prerr_endline "You must supply a program to be analyzed";
+      Arg.usage (Arg.align spec_list) usage_msg
+    end
+  | Some x -> begin
+      CmdLine.run (CfgIr.get_gfile());
+      if !CmdLine.show_stats then Log.print_stats ()
+    end*)
