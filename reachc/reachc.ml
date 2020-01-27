e0a64b4d7a4e44f5e46793bf69cc76a98310250e
Jason Breck <jbreck@cs.wisc.edu>
2020-01-27 13:44:25 -0600

    Removing commented code from reachc.ml

--- reachc/reachc.ml
+++ reachc/reachc.ml
@@ -1,22 +1,5 @@
-(*open Core
-open CfgIr
-open Apak*)
 open Srk
-(*open Safety*)
 
-(* Frontends *)
-(*open TranslateCil
-open TranslateCbp
-open Conversion*)
-
-(* Analyses *)
-(*open Cra
-open Proofspace
-open Dependence
-open ConcDep
-open Chora*)
-
-(*open CmdLine*)
 open Analyzer
 
 let usage_msg = "Cfind program analyzer\nUsage: cfind [OPTIONS] file.smt2"
@@ -24,7 +7,6 @@ let usage_msg = "Cfind program analyzer\nUsage: cfind [OPTIONS] file.smt2"
 let anon_fun s = ignore (CmdLine.main_filename := Some s)
 
 let () =
-  (*Sys.set_signal Sys.sigtstp (Sys.Signal_handle (fun _ -> Log.print_stats ()));*)
   let spec_list = CmdLine.spec_list () in
   Arg.parse (Arg.align spec_list) anon_fun usage_msg;
   match !CmdLine.main_filename with
@@ -34,14 +16,5 @@ let () =
       Arg.usage (Arg.align spec_list) usage_msg
     end
   | Some f -> 
-      CmdLine.run f 
-  (*;
-  match !CfgIr.gfile with
-  | None -> begin
-      prerr_endline "You must supply a program to be analyzed";
-      Arg.usage (Arg.align spec_list) usage_msg
-    end
-  | Some x -> begin
-      CmdLine.run (CfgIr.get_gfile());
-      if !CmdLine.show_stats then Log.print_stats ()
-    end*)
+      CmdLine.run f
+
